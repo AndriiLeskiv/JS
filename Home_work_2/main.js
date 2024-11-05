312ec40f43756f58aa98e81b4bbaa7e08168e3af
AndriiLeskiv <leskivandrii21@gmail.com>
2024-11-05 13:01:25 +0200

    Second Home_work

--- Home_work_2/main.js
+++ Home_work_2/main.js
@@ -225,20 +225,20 @@ let coursesAndDurationArray = [
     {title: 'Frontend', monthDuration: 4}
 ];
 if(coursesAndDurationArray[0].monthDuration > 5){
-    console.log(coursesAndDurationArray[0].title + ' ' + 'Супер');
+    console.log(coursesAndDurationArray[0].title + ' Супер');
 }
 if(coursesAndDurationArray[1].monthDuration > 5){
-    console.log(coursesAndDurationArray[1].title + ' ' + 'Супер');
+    console.log(coursesAndDurationArray[1].title + ' Супер');
 }
 if(coursesAndDurationArray[2].monthDuration > 5){
-    console.log(coursesAndDurationArray[2].title + ' ' + 'Супер');
+    console.log(coursesAndDurationArray[2].title + ' Супер');
 }
 if(coursesAndDurationArray[3].monthDuration > 5){
-    console.log(coursesAndDurationArray[3].title + ' ' + 'Супер');
+    console.log(coursesAndDurationArray[3].title + ' Супер');
 }
 if(coursesAndDurationArray[4].monthDuration > 5){
-    console.log(coursesAndDurationArray[4].title + ' ' + 'Супер');
+    console.log(coursesAndDurationArray[4].title + ' Супер');
 }
 if(coursesAndDurationArray[5].monthDuration > 5){
-    console.log(coursesAndDurationArray[5].title + ' ' + 'Супер');
+    console.log(coursesAndDurationArray[5].title + ' Супер');
 }
