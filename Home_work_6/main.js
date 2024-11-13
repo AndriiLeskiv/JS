86e90c91f205e42d3bc297516420919029b4a494
AndriiLeskiv <leskivandrii21@gmail.com>
2024-11-13 23:45:32 +0200

    [add][HomeWork 6]

--- Home_work_6/main.js
+++ Home_work_6/main.js
@@ -37,3 +37,41 @@ function stringToarray(strNew){
 console.log(stringToarray(strNew));
 
 //- є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map  перетворити всі об'єкти в масиві на стрінгові.
+
+
+//- створити функцію sortNums(array,direction), яка приймає масив чисел, та сортує його від більшого до меншого,
+// або навпаки в залежності від значення аргументу direction.
+let nums = [11,21,3];
+function sortNums(array, direction){
+
+}
+console.log(sortNums(nums,'ascending')) // [3,11,21]
+sortNums(nums,'descending') // [21,11,3]
+
+
+//- є масив
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+//  -- відсортувати його за спаданням за monthDuration
+let sortArray = coursesAndDurationArray.sort( (monthDuration1, monthDuration2) =>{
+    return monthDuration2.monthDuration - monthDuration1.monthDuration;
+})
+console.log(sortArray);
+//  -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+let filteredArray =  coursesAndDurationArray.filter(value => value.monthDuration > 5);
+console.log(filteredArray);
+//  -- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
+let mapArray = coursesAndDurationArray.map(function (value, index){
+    return {
+        id: index + 1,
+        title: value.title,
+        monthDuration: value.monthDuration
+    }
+})
+console.log(mapArray);
