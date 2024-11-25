d0e18d0816839f07c6fa0d02d46ecc454aeef097
AndriiLeskiv <leskivandrii21@gmail.com>
2024-11-26 01:14:45 +0200

    [add][Additional 11]

---
+++ Home_work_11/Task_1/main.js
@@ -0,0 +1,40 @@
+//- взяти https://dummyjson.com/docs/carts та вивести інформацію про всі корзини. Відобразити всі поля кожної корзини.
+fetch('https://dummyjson.com/carts')
+    .then(value => value.json())
+    .then(res => {
+        let {carts} = res;
+
+        let cartsBox = document.createElement('div');
+        cartsBox.classList.add('carts');
+
+        for (let cart of carts) {
+            let cartItem = document.createElement('div');
+            cartItem.classList.add('item');
+            cartItem.innerHTML = `
+                <h2>Cart ID: ${cart.id}</h2>
+                <p>User ID: ${cart.userId}</p>
+                <p>Total Products: ${cart.totalProducts}</p>
+                <p>Total Quantity: ${cart.totalQuantity}</p>
+                <p>Total Price: $${cart.total.toFixed(2)}</p>
+                <p>Discounted Total: $${cart.discountedTotal.toFixed(2)}</p>
+            `;
+
+            for (let product of cart.products) {
+                let productItem = document.createElement('div');
+                productItem.classList.add('productItem');
+                productItem.innerHTML = `
+                    <h2>Product ID: ${product.id}</h2>
+                    <h3>Product Title: ${product.title}</h3>
+                    <p>Product Quantity: ${product.quantity}</p>
+                    <p>Product Discount Percentage: ${product.discountPercentage.toFixed(2)}</p>
+                    <p>Product Price: $${product.price.toFixed(2)}</p>
+                    <p>Product Total: $${product.total.toFixed(2)}</p>
+                    <p>Product Discounted Total: $${product.discountedTotal.toFixed(2)}</p>
+                    <img src="${product.thumbnail}" alt="">
+                `;
+                cartItem.appendChild(productItem);
+            }
+            cartsBox.appendChild(cartItem);
+        }
+        document.body.appendChild(cartsBox);
+    });
---
+++ Home_work_11/Task_2/main.js
@@ -0,0 +1,77 @@
+//- взяти https://dummyjson.com/docs/recipes та вивести інформацію про всі рецепти.
+// Інгредієнти повинні бути список під час відображення.
+fetch('https://dummyjson.com/recipes')
+    .then(value => value.json())
+    .then(res => {
+        let {recipes} = res;
+
+        let recipesBox = document.createElement('div');
+        recipesBox.classList.add('recipes');
+
+        for (let recipe of recipes) {
+            let recipeItem  = document.createElement('div');
+            recipeItem.classList.add('recipe-item');
+            recipeItem.innerHTML = `
+                <h3>Recipes ID: ${recipe.id}</h3>
+                <h2>Recipes Name: ${recipe.name}</h2>
+                <img src="${recipe.image}" alt="${recipe.title}">
+            `;
+
+            let ingredientUl = document.createElement('ul');
+            ingredientUl.classList.add('ingredients-list');
+            ingredientUl.innerHTML = `<h3>Ingredients:</h3>`;
+
+            for (let ingredient of recipe.ingredients) {
+                let ingredientLi = document.createElement('li');
+                ingredientLi.textContent = ingredient;
+                ingredientUl.appendChild(ingredientLi);
+            }
+
+            let instructionsUl = document.createElement('ol');
+            instructionsUl.classList.add('instructions-list');
+            instructionsUl.innerHTML = `<h3>Instructions:</h3>`;
+
+            for (let instruction of recipe.instructions) {
+                let instructionLi = document.createElement('li');
+                instructionLi.textContent = instruction;
+                instructionsUl.appendChild(instructionLi);
+            }
+
+            let otherInformation  = document.createElement('div');
+            otherInformation.classList.add('other-info');
+            otherInformation.innerHTML = `
+                <p>Prep Time Minutes: ${recipe.prepTimeMinutes}</p>
+                <p>Cook Time Minutes: ${recipe.cookTimeMinutes}</p>
+                <p>Servings: ${recipe.servings}</p>
+                <p>Difficulty: ${recipe.difficulty}</p>
+                <p>Calories Per Serving: ${recipe.caloriesPerServing}</p>
+                <p>User Id: ${recipe.userId}</p>
+                <p>Rating: ${recipe.rating}</p>
+                <p>Review Count: ${recipe.reviewCount}</p>
+            `;
+
+            let recipeTags = document.createElement('ul');
+            recipeTags.classList.add('recipe-tags');
+            recipeTags.innerHTML = `<h3>Recipe Tags:</h3>`;
+
+            for (let tag of recipe.tags) {
+                let tagLi = document.createElement('li');
+                tagLi.textContent = tag;
+                recipeTags.appendChild(tagLi);
+            }
+
+            let mealType = document.createElement('ol');
+            mealType.classList.add('meal-type');
+            mealType.innerHTML = `<h3>Meal Type:</h3>`;
+
+            for (let type of recipe.mealType) {
+                let typeLi = document.createElement('li');
+                typeLi.textContent = type;
+                mealType.appendChild(typeLi);
+            }
+
+            recipeItem.append(ingredientUl, instructionsUl, otherInformation, recipeTags, mealType);
+            recipesBox.appendChild(recipeItem);
+        }
+        document.body.appendChild(recipesBox);
+    });
